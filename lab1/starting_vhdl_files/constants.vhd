696fde7c3248bdbbb5cf138ea4424ac3c95a17aa
thegabriele97 <gabriele.lagreca@gmail.com>
2021-03-05 09:34:19 +0100

    Added starting vhdl files for lab1

---
+++ lab1/starting_vhdl_files/constants.vhd
@@ -0,0 +1,11 @@
+package CONSTANTS is
+   constant IVDELAY : time := 0.1 ns;
+   constant NDDELAY : time := 0.2 ns;
+   constant NDDELAYRISE : time := 0.6 ns;
+   constant NDDELAYFALL : time := 0.4 ns;
+   constant NRDELAY : time := 0.2 ns;
+   constant DRCAS : time := 1 ns;
+   constant DRCAC : time := 2 ns;
+   constant NumBit : integer := 4;	
+   constant TP_MUX : time := 0.5 ns; 	
+end CONSTANTS;
---
+++ lab1/starting_vhdl_files/rca.vhd
@@ -0,0 +1,66 @@
+library ieee; 
+use ieee.std_logic_1164.all; 
+use ieee.std_logic_unsigned.all;
+
+entity RCA is 
+	generic (DRCAS : 	Time := 0 ns;
+	         DRCAC : 	Time := 0 ns);
+	Port (	A:	In	std_logic_vector(5 downto 0);
+		B:	In	std_logic_vector(5 downto 0);
+		Ci:	In	std_logic;
+		S:	Out	std_logic_vector(5 downto 0);
+		Co:	Out	std_logic);
+end RCA; 
+
+architecture STRUCTURAL of RCA is
+
+  signal STMP : std_logic_vector(5 downto 0);
+  signal CTMP : std_logic_vector(6 downto 0);
+
+  component FA 
+  generic (DFAS : 	Time := 0 ns;
+           DFAC : 	Time := 0 ns);
+  Port ( A:	In	std_logic;
+	 B:	In	std_logic;
+	 Ci:	In	std_logic;
+	 S:	Out	std_logic;
+	 Co:	Out	std_logic);
+  end component; 
+
+begin
+
+  CTMP(0) <= Ci;
+  S <= STMP;
+  Co <= CTMP(6);
+  
+  ADDER1: for I in 1 to 6 generate
+    FAI : FA 
+	  generic map (DFAS => DRCAS, DFAC => DRCAC) 
+	  Port Map (A(I-1), B(I-1), CTMP(I-1), STMP(I-1), CTMP(I)); 
+  end generate;
+
+end STRUCTURAL;
+
+
+architecture BEHAVIORAL of RCA is
+
+begin
+  
+  S <= (A + B) after DRCAS;
+  
+end BEHAVIORAL;
+
+configuration CFG_RCA_STRUCTURAL of RCA is
+  for STRUCTURAL 
+    for ADDER1
+      for all : FA
+        use configuration WORK.CFG_FA_BEHAVIORAL;
+      end for;
+    end for;
+  end for;
+end CFG_RCA_STRUCTURAL;
+
+configuration CFG_RCA_BEHAVIORAL of RCA is
+  for BEHAVIORAL 
+  end for;
+end CFG_RCA_BEHAVIORAL;
