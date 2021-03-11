20bee473d54473229bf999f12503de5411dbd07f
Matteo Battilana <matteo@matteobattilana.com>
2021-03-11 14:34:07 +0100

    moved to strcural fd_generic

--- lab1/syn/fd_generic.vhd
+++ lab1/syn/fd_generic.vhd
@@ -1,58 +1,43 @@
 library IEEE;
-use IEEE.std_logic_1164.all; 
+use IEEE.std_logic_1164.all;
 use WORK.constants.all;
 
-entity FD_GENERIC is
-	generic (NBIT : integer := NumBit);
-	Port (	D:	In	std_logic_vector(NBIT-1 downto 0);
-		CK:	In	std_logic;
-		RESET:	In	std_logic;
-		ENABLE:	In	std_logic;
-		Q:	Out	std_logic_vector(NBIT-1 downto 0));
-end FD_GENERIC;
+entity fd_generic is
+	Generic (NBIT: integer:= numBit);
+	Port (	D:	In	std_logic_vector(NBIT - 1 downto 0);
+					CK:	In	std_logic;
+					RESET:	In	std_logic;
+					ENABLE: In std_logic;
+					Q:	Out	std_logic_vector(NBIT - 1 downto 0));
+end fd_generic;
 
 
-architecture PIPPO_GENERIC of FD_GENERIC is -- flip flop D with syncronous reset
+architecture structural of fd_generic is
+	component FD is
+		Port (	D:	In	std_logic;
+						CK:	In	std_logic;
+						RESET:	In	std_logic;
+						Q:	Out	std_logic);
+	end component;
 
-begin
-	PSYNCH: process(CK,RESET)
-	begin
-	  if CK'event and CK='1' then -- positive edge triggered:
-	    if RESET='1' then -- active high reset 
-	      Q <= (others => '0'); 
-	    elsif ENABLE = '1' then
-	      Q <= D; -- input is written on output
-	    end if;
-	  end if;
-	end process;
-
-end PIPPO_GENERIC;
-
-architecture PLUTO_GENERIC of FD_GENERIC is -- flip flop D with asyncronous reset
-
-begin
-	
-	PASYNCH: process(CK,RESET)
-	begin
-	  if RESET='1' then
-	    Q <= (others => '0');
-	  elsif CK'event and CK='1' and ENABLE = '1' then -- positive edge triggered:
-	    Q <= D; 
-	  end if;
-	end process;
-
-end PLUTO_GENERIC;
+	fds: for i in 0 to NBIT - 1 generate
+			fdi: FD port map(D => D(i), CK => (CK AND ENABLE), RESET => RESET, Q => Q(i));
+	end generate fds;
+end struct_sync;
 
 
-configuration CFG_FD_GEN_PIPPO of FD_GENERIC is
-	for PIPPO_GENERIC
+configuration CFG_FD_GENERIC_SYNC of fd_generic is
+	for structural
+				for all: FD
+						use configuration WORK.CFG_FD_PIPPO;
+				end for;
 	end for;
-end CFG_FD_GEN_PIPPO;
+end CFG_IV_GENERIC_BEHAVIORAL;
 
-
-configuration CFG_FD_GEN_PLUTO of FD_GENERIC is
-	for PLUTO_GENERIC
-	end for;
-end CFG_FD_GEN_PLUTO;
-
-
+configuration CFG_FD_GENERIC_ASYNC of fd_generic is
+	for structural
+			for all: FD
+					use configuration WORK.CFG_FD_PLUTO;
+			end for;
+		end for;
+end CFG_IV_GENERIC_STRUCTURAL;
