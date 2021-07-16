3a1ef9cc3e6f651ff2aa2595983ffb8ad003192b
thegabriele97 <gabriele.lagreca@gmail.com>
2021-07-16 16:35:19 +0200

    DLX-Delivery: updated files

--- DLX/Delivery/GR16_DLX_pro/AsmExamples/JumpTest.asm
+++ DLX/Delivery/GR16_DLX_pro/AsmExamples/JumpTest.asm
@@ -1,20 +1,20 @@
 
 
 main:
-    j a
-    addi r1, r0, #2
+    j a                 ;0
+    addi r1, r0, #2     ;4
 
 a:
-    jal b
-    addi r1, r0, #3
+    jal b               ;8
+    addi r1, r0, #3     ;C
     
 b:
-    addi r24, r0, c
-    jr r24
-    addi r1, r0, #4
+    addi r24, r0, c     ;10
+    jr r24              ;14
+    addi r1, r0, #4     ;18
 
 c:
-    addi r15, r0, b
-    jalr r15
-    addi r1, r0, #5
+    addi r15, r0, b     ;1C
+    jalr r15            ;20
+    addi r1, r0, #5     ;24
 
